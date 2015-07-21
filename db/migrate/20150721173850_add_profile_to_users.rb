615eddd379ca7c40d70ada15923e075a3c13ac5d
estherm <esther_mun@hotmail.com>
2015-07-21 14:19:09 -0400

    Revert "before migrating profile to users"
    This reverts commit 22bf9246cc4077784826c0a19d69d6206c4456c7.

--- db/migrate/20150721173850_add_profile_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddProfileToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :name, :string
-  	add_column :users, :username, :string
-  	add_column :users, :description, :string
-  end
-end
