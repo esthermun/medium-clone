4f441b051bca79f09d55a862a8d6df0773da2c9c
estherm <esther_mun@hotmail.com>
2015-07-21 11:15:04 -0400

    show the links on the page
    showing links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  get '/secret' => 'home#secret'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
