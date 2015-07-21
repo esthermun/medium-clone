838d2352aabfc51b807afae69949ccc2dd91561d
estherm <esther_mun@hotmail.com>
2015-07-21 14:02:51 -0400

    migrated adding profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721150315) do
+ActiveRecord::Schema.define(version: 20150721173850) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20150721150315) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "username"
+    t.string   "description"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
