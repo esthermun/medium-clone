17cc4255e2c4c5560a0e38523b5bdeb5144f9a36
estherm <esther_mun@hotmail.com>
2015-07-27 02:21:46 -0400

    models settings done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+
+  has_one :profile, :through => :user
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ActiveRecord::Base
 
 	has_one :profile, :through => :user
 
+	has_many :comments
+
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
 
