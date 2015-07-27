08bc435df321bc65a51e807da330cff8f4954456
estherm <esther_mun@hotmail.com>
2015-07-27 02:50:25 -0400

    creating form for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+	def create
+		@post = Post.find(params[:id])
+		@comment = @post.comments.create(comment_params)
+		@comment.user_id = current_user.id
+		if @comment.save
+			flash[:notice] = "New comment successfully posted"
+			redirect_to @post
+		end
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:content)
+	end
 end
